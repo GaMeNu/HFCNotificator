a0b3832c25bdb1d194d7d5435897c55b112b25a4
GaMeNu <98153342+GaMeNu@users.noreply.github.com>
2024-11-06 19:20:26 +0200

    v2.2.6: fix: Extra security layer for duped loops
    Loops should now automatically terminate themselves if they detect that the time between the loops running is too quick.

--- alert_maker.py
+++ alert_maker.py
@@ -84,7 +84,7 @@ class AlertEmbed:
             self.embed.title = f'התראה ב{self.district}'
             self.embed.add_field(name=self.alert.title, value='')
 
-        self.embed.add_field(name='נכון ל', value=datetime.datetime.now().strftime("%H:%M:%S\n%d/%m/%Y"), inline=False)
+        self.embed.add_field(name='זמן התראה', value=datetime.datetime.now().strftime("%H:%M:%S\n%d/%m/%Y"), inline=False)
         self.embed.add_field(name='מידע נוסף', value=self.alert.description)
 
     @classmethod

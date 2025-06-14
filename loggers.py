56f984df62ae11ab27f815c1e99e41309adc2b9e
GaMeNu <98153342+GaMeNu@users.noreply.github.com>
2025-06-14 21:46:33 +0300

    v2.2.6.1 fix log files if missing

--- loggers.py
+++ loggers.py
@@ -33,6 +33,12 @@ class ColorFormatter(discord.utils._ColourFormatter):
 
 class DefaultFileHandler(logging.FileHandler):
     def __init__(self, filename: str):
+        path = Path(LOGGING_DIR, filename)
+
+        # Make sure the damn file exists
+        if not (path.exists() or path.is_file()):
+            path.touch()
+
         super().__init__(Path(LOGGING_DIR, filename))
         self.setFormatter(logging.Formatter(
             "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
